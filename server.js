8df3f49853a0b04c3997827627a9b8b5effb1712
Gowtham1999 <30837374+Gowtham1999@users.noreply.github.com>
2017-08-09 20:08:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('ui/articleone',function(req,res){
+   res.send("articleone is executed"); 
+});
+app.get('ui/articletwo',function(req,res){
+   res.send("articletwo is executed"); 
+});
+app.get('ui/articlethree',function(req,res){
+   res.send("articlethree is executed"); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
