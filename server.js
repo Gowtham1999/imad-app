0c77a9b0501c3a501f48529b370800826282cde6
Gowtham1999 <30837374+Gowtham1999@users.noreply.github.com>
2017-08-28 21:56:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,77 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var cont = {
+     articleone:` <div class="block">
+     <h1>Articleone</h1>
+     <div>19 Jan 1999</div>
+     <p>
+       This is the content of the article one.
+       Its just the beginning.
+     </p>
+     <p>
+       This is the content of the article one.
+       Its just the beginning.
+     </p>
+     <p>
+       This is the content of the article one.
+       Its just the beginning.
+     </p>
+     <p>
+       This is the content of the article one.
+       <br/>
+       Its just the beginning.
+     </p>
+     </div>`,
+     articletwo:` <div class="block">
+     <h1>Articleone</h1>
+     <div>19 Jan 1999</div>
+     <p>
+       This is the content of the article one.
+       Its just the beginning.
+     </p>
+     <p>
+       This is the content of the article one.
+       Its just the beginning.
+     </p>
+     <p>
+       This is the content of the article one.
+       Its just the beginning.
+     </p>
+     <p>
+       This is the content of the article one.
+       <br/>
+       Its just the beginning.
+     </p>
+     </div> `
+     
+};
 
+function Createtemplate (data){
+    var cont=data.cont;
+    var htmltempate = `<!DOCTYPE html>
+                     <html>
+                     <head>
+                <title>Articleone</title>
+     <meta name="viewport" content="width=device-width,initial-scale=1"/>
+     <link href="/ui/style.css" rel="stylesheet" />
+ </head> 
+
+ <body>
+     <div>
+         <a href="/">Homepage</a>
+     </div>
+     <hr/> 
+ </body>
+ </html>`;
+ return htmlcontent;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articleone',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'articleone.html')); 
+app.get('/:article',function(req,res){
+    var article=req.params.article;
+   res.send(Createtemplate(cont[article])); 
 });
 
 app.get('/ui/style.css', function (req, res) {
