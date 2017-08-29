505fba3040f326867de57e01064567ce5c53683b
Gowtham1999 <30837374+Gowtham1999@users.noreply.github.com>
2017-08-29 06:13:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 var margin = 0;
 function moveright(){
-    margin = margin + 10;
+    margin = margin + 5;
     img.style.marginLeft=margin + "px";
 }
 img.onclick = function(){
-  var interval = setInterval(moveright,70);
+  var interval = setInterval(moveright,50);
 };
