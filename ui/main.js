b53fa324f0059991e34dc93a68e5993edb0dba82
Gowtham1999 <30837374+Gowtham1999@users.noreply.github.com>
2017-08-29 06:19:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 var margin = 0;
 function moveright(){
-    margin = margin + 0.01;
+    margin = margin + 0.1;
     img.style.marginLeft=margin + "px";
 }
 img.onclick = function(){
